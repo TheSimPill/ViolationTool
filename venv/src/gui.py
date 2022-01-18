c89321dfb725cc5165de6d95463f95c095f6a99f
Freddie Almond <freddiealmond@gmai.com>
2022-01-18 14:37:29 -0500

    Fixed gui.py and started progress bar

--- venv/src/gui.py
+++ venv/src/gui.py
@@ -1,49 +1,54 @@
 import tkinter as tk
 from tkinter.filedialog import askdirectory
 from PIL import Image, ImageTk
-import sys, os
-
-def resource_path(relative_path):
-    """ Get the absolute path to the resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
+from tkinter.ttk import Progressbar
 
 root = tk.Tk()
 root.title("NHI Scraper")
-startpage = tk.Frame(root, width=600, height=400)
-# Splits canvas into 3 identical invisible elements
-startpage.grid(columnspan=3, rowspan=3)
+root.iconbitmap(r"C:\Users\FreddieG3\Documents\Job\Impruvon\Web Scraper Project GUI\venv\src\icon.ico")
+root.resizable(width=False, height=False)
+root.geometry("500x300")
 
 # Logo
 logo = Image.open(r"C:\Users\FreddieG3\Documents\Job\Impruvon\Web Scraper Project GUI\venv\src\logo.png")
 logo = ImageTk.PhotoImage(logo)
 logo_label = tk.Label(image=logo)
 logo_label.image = logo
-logo_label.grid(column=1, row=0)
+logo_label.grid(column=1, row=0, columnspan=3)
 
 # Instructions
-instructions = tk.Label(root, text="Choose path of folder to save required files to \nOR\n Choose path to required files from previous run", font="Times")
-instructions.grid(columnspan=3, column=0, row=1)
+instructions = tk.Label(root, text="Download Nursing Home Inspect's Raw data?", font=("Times", 15))
+instructions.grid(column=1, row=1, columnspan=3, pady=10)
 
-def open_file():
-    browse_text.set("loading...")
-    dir = askdirectory()
-    browse_text.set("Browse")
-
-# Browse button
+# Yes button
 browse_text = tk.StringVar()
-browse_btn = tk.Button(root, command=lambda:open_file(), textvariable=browse_text, font="Times", bg="#000099", fg="#00ace6", height=2, width=15)
-browse_text.set("Browse")
-browse_btn.grid(column=1, row=2)
+yes_btn = tk.Button(root, command=lambda:open_file(),textvariable=browse_text, font="Times", bg="#000099", fg="#00ace6", height=2, width=15)
+browse_text.set("Yes")
+yes_btn.grid(column=1, row=2, pady=10)
 
-# Add margin under button
-canvas = tk.Canvas(root, width=600, height=250)
-canvas.grid(columnspan=3)
+# No button
+browse_text = tk.StringVar()
+no_btn = tk.Button(root, textvariable=browse_text, font="Times", bg="#000099", fg="#00ace6", height=2, width=15)
+browse_text.set("No")
+no_btn.grid(column=3, row=2, pady=10)
+
+# If yes is clicked
+def open_file():
+    dir = askdirectory()
+    yes_btn.grid_forget()
+    no_btn.grid_forget()
+    instructions.config(text="Starting download")
+
+    # Progress bar widget
+    progress = Progressbar(root, orient = "horizontal",
+                length = 200, mode = 'determinate')
+    progress.grid(column=1, row=2, columnspan=3, pady=10)
+
+# Progress bar function
+def bar():
+    pass
+
+
+
 
 root.mainloop()
