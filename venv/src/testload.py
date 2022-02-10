4097ee18bdcc0995d1c00f80ecc54cbf98a8a8ac
Freddie Almond <falmond@umd.edu>
2022-02-10 13:35:57 -0500

    Testing pandas

---
+++ venv/src/testload.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from os import listdir
+
+xclf = listdir(r"C:\Users\FreddieG3\Documents\Job\Impruvon\Web Scraper Project GUI\venv\src\rawdata")
+dfs = []
+for wb in xclf:
+    pd.read_excel()
