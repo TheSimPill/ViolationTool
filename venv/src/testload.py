371e3e3198d9a6196e99000f1f0bf497beac55bd
Frederick Almond <falmond@umd.edu>
2022-02-10 15:16:35 -0500

    Pandas init

--- venv/src/testload.py
+++ venv/src/testload.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from os import listdir
 
-xclf = listdir(r"C:\Users\FreddieG3\Documents\Job\Impruvon\Web Scraper Project GUI\venv\src\rawdata")
-dfs = []
-for wb in xclf:
-    pd.read_excel()
+file = r"/Users/Freddie/Impruvon/guiwebscraperproject/venv/rawdata/text2567_20211001_cms_reg1.xlsx"
+df = pd.read_excel(file, usecols="A,E,G,H,I", names=["Organization", "State", "Date", "Tag", "Severity"])
+
+
+print(df.head())
+df.insert(5, "Fine", 0)
+df.insert(6, "Url", 0)
+print(df.head())
